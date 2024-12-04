8d83ca39d4767ea2b69d55354f35e6139c397818
CasteAwayy <140751958+CasteAwayy@users.noreply.github.com>
2024-12-04 17:35:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
+const strongWords = document.querySelectorAll('strong');
 function highlight() {
-    //Write your code here
-
+  //Write your code here
+ strongWords.forEach((ele)=>{
+    ele.style.color = 'green';
+ })
 
 }
 
-
 function return_normal() {
-    //Write your code here
-
-    
+  //Write your code here
+  strongWords.forEach((ele)=>{
+    ele.style.color = 'black';
+ })
+  
 }
